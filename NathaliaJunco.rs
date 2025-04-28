ac5f878ea99597c015e5a14c5f377df056ca1d23
20JuaN00 <20juandiego00@gmail.com>
2025-04-28 11:48:30 -0500

    main

--- NathaliaJunco.rs
+++ NathaliaJunco.rs
@@ -1,1 +1,58 @@
-pkgswitch.nathalia.junco.SwitchNathaliaJunco
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+ package pkgswitch.nathalia.junco;
+
+ import java.util.Scanner;
+ 
+ /**
+  *
+  * @author Estudiantes
+  */
+ public class SwitchNathaliaJunco {
+ 
+     /**
+      * @param args the command line arguments
+      */
+     public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+         
+         System.out.println("ingrese el caso a escanear: ");
+                 
+         int dia = scanner.nextInt();
+         
+         switch (dia) {                
+               case 1:
+                   
+                     System.out.println("verano");
+                     break;
+                     
+                case 2:
+                   
+                     System.out.println("otoño");
+                     break;
+                     
+                 case 3:
+                   
+                     System.out.println("invierno" );
+                     break;    
+                     
+                 case 4:
+                   
+                     
+                  default:
+                      System.out.println("numero invalido" );
+                     break;
+         }    
+     }    
+                     
+     
+                 
+               
+                     
+                 
+                 
+                     
+     
+ }
